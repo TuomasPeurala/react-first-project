f192cb1de871147e185aca5c855118d87a53a1ca
TuomasPeurala <peuralatuomas@gmail.com>
2024-09-09 15:18:18 +0300

    toinen tehtävä

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>Hello World!</h2>
+      <ExpenseItem></ExpenseItem>
       <p>Tuomas / VAMK / BIT</p>
       </div>
   );

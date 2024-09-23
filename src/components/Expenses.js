57727e94b1e6a1d227bba75dc8ded96d883b3bc5
TuomasPeurala <peuralatuomas@gmail.com>
2024-09-23 14:52:51 +0300

    Tehtävän 5 aloitusta

---
+++ src/components/Expenses.js
@@ -0,0 +1,12 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function Expenses(props) {
+    return (
+      <div className="expenses"> {/* Käytä expenses-luokkaa */}
+        
+      </div>
+    );
+  }
+  
+  export default Expenses;
